deb6eec7879d6c904422e4d098bec0141869485d
Tony Spunei <tony.spunei@yahoo.com>
2024-02-09 18:20:22 +0100

    init custom start

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [sveltekit()],
+  define: {
+    'process.env': process.env
+  },
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  },
+  resolve: {
+    alias: {
+      $: path.resolve(__dirname, './src')
+    }
+  }
+});
