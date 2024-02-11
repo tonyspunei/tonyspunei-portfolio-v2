fe8f1e766ff3765327a723f68add9b4c4e6bce1a
Tony Spunei <tony.spunei@yahoo.com>
2024-02-11 19:51:59 +0100

    add adapterNetlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterNetlify from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 /** @type {import('@sveltejs/kit').Config} */
 export default {
   kit: {
-    adapter: adapter()
+    adapter: adapterNetlify({split: false})
   },
   preprocess: vitePreprocess()
 };
